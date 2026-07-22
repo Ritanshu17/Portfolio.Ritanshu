3a3a5879ff93bebb5c926bf43d8584376d22ac6d
Ritanshu17 <ritanshu755mankar@gmail.com>
2026-07-22 22:04:41 +0530

    update the App.jsx file by importing contact file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Skills from "./components/Skills";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+import Contact from "./components/contact";
 import Footer from "./components/Footer";
 import Highlights from "./components/Highlights";
 import ScrollProgress from "./components/ScrollProgress";
